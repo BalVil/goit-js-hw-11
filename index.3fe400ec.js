b6254d39fe0707d1f60c6ed76d42267098c14ec1
BalVil <93622460+BalVil@users.noreply.github.com>
2022-05-31 00:12:48 +0000

    Deploying to gh-pages from @ BalVil/goit-js-hw-11@1c408763214bcd5b1064b02fc58c5a6524337978 🚀

---
+++ index.3fe400ec.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();const t=e.currentTarget.elements.searchQuery.value;console.log(t)}));
+//# sourceMappingURL=index.3fe400ec.js.map
---
+++ index.77dba4f9.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();var r=e.currentTarget.elements.searchQuery.value;console.log(r)}));
+//# sourceMappingURL=index.77dba4f9.js.map
