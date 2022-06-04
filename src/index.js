f5e3976a6f25c9b16b5ecbb2fe2623bbef037a0c
BalVil <balutavili@gmail.com>
2022-06-05 01:19:54 +0300

    async/await + a little of "infinite scrolling"

--- src/index.js
+++ src/index.js
@@ -14,11 +14,12 @@ const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 
 // ====Listeners=========
 searchFormRef.addEventListener('submit', onSearch);
-loadMoreBtn.button.addEventListener('click', onLoadMoreBtn);
+// loadMoreBtn.button.addEventListener('click', onLoadMoreBtn);
 
 let simpleLightbox = null;
+let lastImg = null;
 
-function onSearch(evt) {
+async function onSearch(evt) {
   evt.preventDefault();
 
   imagesApi.query = evt.currentTarget.elements.searchQuery.value;
@@ -31,41 +32,49 @@ function onSearch(evt) {
   imagesApi.resetPage();
   clearImagesBox();
 
-  imagesApi
-    .fetchImages()
-    .then(({ hits, totalHits }) => {
-      if (!hits.length) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
+  try {
+    const { hits, totalHits } = await imagesApi.fetchImages();
 
-        loadMoreBtn.show();
-        createImagesBox(hits);
-        makeSimpleLightbox();
-      }
-    })
-    .catch(error => console.log(error.message));
-}
+    if (!hits.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
 
-function onLoadMoreBtn() {
-  imagesApi
-    .fetchImages()
-    .then(({ hits }) => {
+      // loadMoreBtn.show();
       createImagesBox(hits);
-      refreshSimpleLightbox();
-      smoothScrolling('.container');
+      makeSimpleLightbox();
 
-      if (hits.length < imagesApi.per_page) {
-        Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-        return loadMoreBtn.hide();
+      lastImg = document.querySelector('.simplelightbox-gallery:last-child');
+
+      if (lastImg) {
+        infiniteObserver.observe(lastImg);
       }
-    })
-    .catch(error => console.log(error.message));
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
+// async function onLoadMoreBtn() {
+//   try {
+//     const { hits } = await imagesApi.fetchImages();
+
+//     createImagesBox(hits);
+//     refreshSimpleLightbox();
+//     // smoothScrolling('.container');
+
+//     if (hits.length < imagesApi.per_page) {
+//       Notify.failure(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//       return loadMoreBtn.hide();
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 
 function clearImagesBox() {
   galleryRef.innerHTML = '';
@@ -115,13 +124,27 @@ function refreshSimpleLightbox() {
   simpleLightbox.refresh();
 }
 
-function smoothScrolling(selector) {
-  const { height: cardHeight } = document
-    .querySelector(selector)
-    .firstElementChild.getBoundingClientRect();
+// function smoothScrolling(selector) {
+//   const { height: cardHeight } = document
+//     .querySelector(selector)
+//     .firstElementChild.getBoundingClientRect();
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
+//   window.scrollBy({
+//     top: cardHeight,
+//     behavior: 'smooth',
+//   });
+// }
+
+// infinite scrolling
+
+const infiniteObserver = new IntersectionObserver(
+  async ([entry], observer) => {
+    if (entry.isIntersecting) {
+      const { hits, totalHits } = await imagesApi.fetchImages();
+      createImagesBox(hits);
+      observer.unobserve(entry.target);
+      lastImg = document.querySelector('.simplelightbox-gallery:last-child');
+    }
+  },
+  { threshold: 0.5 }
+);
